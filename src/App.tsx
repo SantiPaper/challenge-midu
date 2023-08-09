4f2d545e3f90ee616a31b9ab2c3ea6f52d82bd52
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-08 22:56:46 -0300

    create function addbooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { StyledContainer } from "./style";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { useEffect } from "react";
+import { getBooks } from "./api";
+import { useBookContext } from "./hooks/useBookContext";
 
 function App() {
+  const { addBooks } = useBookContext();
+  useEffect(() => {
+    getBooks().then((data) => addBooks(data));
+  }, []);
+
   return (
     <>
       <StyledContainer>
