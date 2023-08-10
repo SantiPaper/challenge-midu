39253aa2e446c925d3fb87a340189644d2586e42
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-10 19:16:23 -0300

    change types

--- src/types/context.ts
+++ src/types/context.ts
@@ -3,16 +3,25 @@ import { Book } from "./books";
 export type Context = {
   state: State;
   addBooks: (books: Book[]) => void;
+  getByGenre: (genre: Genre) => void;
 };
+
+export type Genre = "Ciencia ficción" | "Fantasía" | "Terror" | "Zombies" | "";
 
 export type State = {
   books: Book[];
   readingList: Book[];
+  filteredBooks: Book[];
 };
 
 export type Reducer = (state: State, action: Action) => State;
 
-export type Action = {
-  type: "ADD_BOOKS";
-  payload: Book[];
-};
+export type Action =
+  | {
+      type: "ADD_BOOKS";
+      payload: Book[];
+    }
+  | {
+      type: "GET_BY_GENRE";
+      payload: Book[];
+    };
