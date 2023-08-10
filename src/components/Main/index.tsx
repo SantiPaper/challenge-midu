b91fc180ce407ca0e9d3b7fdabfdab4ca08931a4
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-10 19:15:39 -0300

    add filters in  main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,13 @@
 import { StyledMain } from "./style";
 import { Hero } from "../Hero";
 import { Books } from "../Books";
+import { Filters } from "../Filters";
 
 export const Main = () => {
   return (
     <StyledMain>
       <Hero />
+      <Filters />
       <Books />
     </StyledMain>
   );
