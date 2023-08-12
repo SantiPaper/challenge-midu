4b89a56ac72261271efa4943a768d7ebca6a149a
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-12 16:59:20 -0300

    style filters

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,6 +1,7 @@
 import { useBookContext } from "../../hooks/useBookContext";
 import { ChangeEvent } from "react";
 import { Genre } from "../../types/context";
+import { StyledSection } from "./style";
 
 const genres: Genre[] = [
   "",
@@ -18,17 +19,27 @@ export const Filters = () => {
   };
 
   return (
-    <form>
-      <label htmlFor="Búsqueda">Buscar libro</label>
-      <input aria-label="Buscar libro por nombre" type="text" id="Búsqueda" />
-      <label htmlFor="Genero">Filtrar por genero</label>
-      <select onChange={onSelect} name="" id="Gereno">
-        {genres.map((genre) => (
-          <option key={genre} value={genre}>
-            {genre || "Todos"}
-          </option>
-        ))}
-      </select>
-    </form>
+    <StyledSection>
+      <form className="form">
+        <div>
+          <label htmlFor="Búsqueda">Buscar libro</label>
+          <input
+            aria-label="Buscar libro por nombre"
+            type="text"
+            id="Búsqueda"
+          />
+        </div>
+        <div>
+          <label htmlFor="Genero">Filtrar por genero</label>
+          <select onChange={onSelect} name="" id="Gereno">
+            {genres.map((genre) => (
+              <option key={genre} value={genre}>
+                {genre || "Todos"}
+              </option>
+            ))}
+          </select>
+        </div>
+      </form>
+    </StyledSection>
   );
 };
---
+++ src/components/Filters/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+  display: flex;
+  justify-content: space-around;
+  .form {
+    flex: 1;
+    display: flex;
+    justify-content: space-around;
+  }
+`;
