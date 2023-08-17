641682d45fb7d8d1ed293bdca11e645e951a173f
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-17 12:20:43 -0300

    update book component

--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -3,17 +3,18 @@ import { StyleBookSection } from "./style";
 import type { Book } from "../../types/books";
 import { BookDetail } from "../BookDetail";
 import { useState } from "react";
+import { BooksMap } from "../BooksMap";
 
-export const Books = () => {
-  const { state, addToLectureList, removeFromLectureList, setDetail } =
-    useBookContext();
+type Props = {
+  value: Book[];
+};
 
+export const Books = ({ value }: Props) => {
+  const { setDetail } = useBookContext();
   const [showDetail, setShowDetail] = useState(false);
 
-  const onClick = (book: Book) => {
-    if (state.readingList.find((b) => b.ISBN === book.ISBN)) {
-      removeFromLectureList(book);
-    } else addToLectureList(book);
+  const closeModal = () => {
+    setShowDetail(false);
   };
 
   const openModal = (book: Book) => {
@@ -21,43 +22,12 @@ export const Books = () => {
     setShowDetail(true);
   };
 
-  const closeModal = () => {
-    setShowDetail(false);
-  };
-
   return (
     <StyleBookSection>
-      <h2 className="books__length">
-        Libros disponibles {state.filteredBooks.length}
-      </h2>
+      <h2 className="books__length">Libros disponibles {value.length}</h2>
       <div className="container__books">
-        {state.filteredBooks.map((book) => (
-          <article className="book__container" key={book.ISBN}>
-            <img className="book__image" src={book.cover} width={120} alt="" />
-            <div className="book__card">
-              <h2 className="book__title">{book.title}</h2>
-              <div className="book__container-info">
-                <p>{book.author.name}</p>
-                <p>{book.year}</p>
-              </div>
-              <div className="book__container-buttons">
-                <button
-                  onClick={() => onClick(book)}
-                  className="book__button-ls"
-                >
-                  {state.readingList.find((b) => b.ISBN === book.ISBN)
-                    ? "Quitar de la lista de lectura"
-                    : "Agregar a la lista de lectura"}
-                </button>
-                <button
-                  className="book__button-wm"
-                  onClick={() => openModal(book)}
-                >
-                  Ver más
-                </button>
-              </div>
-            </div>
-          </article>
+        {value.map((book) => (
+          <BooksMap book={book} openModal={openModal} />
         ))}
         {showDetail && <BookDetail onClose={closeModal} />}
       </div>
