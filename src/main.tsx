3073a5ecf2b759889ce98d34ababf8bc4620f226
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-17 12:14:54 -0300

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "./context/index.tsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
